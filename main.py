88ec5c787ec5caba6004f4f79ba7a3815063a23c
linnkhant07 <lkty07@gmail.com>
2024-09-21 16:36:16 -0700

    base model extracts relationships

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key = openai_api_key)
 
+#create an assistant that utilizes file_search tool (for reading pdfs)
 assistant = client.beta.assistants.create(
   name="Text Relationships Extractor",
   instructions="You are an assistant that extracts specific relationships from research papers and provides output in a structured JSON format.",
@@ -17,9 +18,13 @@ assistant = client.beta.assistants.create(
 
 # Create a vector store 
 vector_store = client.beta.vector_stores.create(name="Clinical Research Papers")
+
+article1 = "Articles/fava-et-al-2008-difference-in-treatment-outcome-in-outpatients-with-anxious-versus-nonanxious-depression-a-star_d-report (1).pdf"
+article2 = "Articles/100-Papers-in-Clinical-Psychiatry-Depressive-Disorders-Comparative-efficacy-and-acceptability-of-12-new-generation-antidepressants-a-multiple-treatments-meta-analysis.pdf"
+article3 = "Articles/WJCC-9-9350.pdf"
  
 # Ready the files for upload to OpenAI
-file_paths = ["Articles/fava-et-al-2008-difference-in-treatment-outcome-in-outpatients-with-anxious-versus-nonanxious-depression-a-star_d-report (1).pdf"]
+file_paths = [article1, article2, article3]
 file_streams = [open(path, "rb") for path in file_paths]
  
 # Use the upload and poll SDK helper to upload the files, add them to the vector store,
@@ -28,66 +33,55 @@ file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
   vector_store_id=vector_store.id, files=file_streams
 )
  
-# You can print the status and the file counts of the batch to see the result of this operation.
+# print the status and the file counts of the batch to see the result of this operation.
 print(file_batch.status)
 print(file_batch.file_counts)
 
 
-# Upload the user provided file to OpenAI
-message_file = client.files.create(
-  file=open("Articles/fava-et-al-2008-difference-in-treatment-outcome-in-outpatients-with-anxious-versus-nonanxious-depression-a-star_d-report (1).pdf", "rb"), purpose="assistants"
+#connect the assistant with the vector store
+assistant = client.beta.assistants.update(
+  assistant_id=assistant.id,
+  tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
 )
- 
-# Create a thread and attach the file to the message
-thread = client.beta.threads.create(
-  messages=[
-    {
-      "role": "user",
-      "content": f"""
-                Thoroughly extract the following relationships from the text:
-                - Relationships betweens symptoms and diseases
-                - Relationships between medications and outcomes.
-                - Relationships between symptoms and treatments.
-                - Relationships between medications and side effects.
-                I want as many useful relationships as possible. 
-                Return the result in the following JSON format:
 
+# a Thread represents a conversation between a user and one or many Assistants.
+thread = client.beta.threads.create()
+
+# we can add a message or messages to the thread
+message = client.beta.threads.messages.create(
+  thread_id=thread.id,
+  role="user",
+  content= f"""
+                Thoroughly extract the following relationships from the clinical research papers that have been provided:
+                - Relationships betweens each symptom and disease
+                - Relationships between each symptom and its treatment.
+                - Relationships between each medication and the mental health problem it is effective against.
+                - Relationships between each medication and its side-effect if available.
+                
+
+                Return the result in the following JSON format with at least 20 relationships:
                 [
                     {{
                         "subject": "Entity1",
                         "relationship": "RelationshipType",
                         "object": "Entity2"
                     }}
-                ]""",
-      # Attach the new file to the message.
-      "attachments": [
-        { "file_id": message_file.id, "tools": [{"type": "file_search"}] }
-      ],
-    }
-  ]
+                ]
+                The output should just be in JSON. Do not add any additional words or messages
+                """
 )
- 
-# The thread now has a vector store with that file in its tool resources.
-print(thread.tool_resources.file_search)
 
-
-# Use the create and poll SDK helper to create a run and poll the status of
-# the run until it's in a terminal state.
-
+# Once all the user Messages have been added to the Thread, you can Run the Thread with any Assistant.
 run = client.beta.threads.runs.create_and_poll(
-    thread_id=thread.id, assistant_id=assistant.id
+  thread_id=thread.id,
+  assistant_id=assistant.id,
 )
 
-messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+if run.status == 'completed': 
+    messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
 
-message_content = messages[0].content[0].text
-annotations = message_content.annotations
-citations = []
-for index, annotation in enumerate(annotations):
-    message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
-    if file_citation := getattr(annotation, "file_citation", None):
-        cited_file = client.files.retrieve(file_citation.file_id)
-        citations.append(f"[{index}] {cited_file.filename}")
+    message_content = messages[0].content[0].text
+    print(message_content.value)
+else:
+  print(run.status)
 
-print(message_content.value)
-print("\n".join(citations))
